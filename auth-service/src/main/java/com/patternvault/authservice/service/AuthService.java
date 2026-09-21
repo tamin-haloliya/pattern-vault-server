d1dc4ebf9ff219b8e30e62e9ad25ed20cc7c6844
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-09-21 21:10:16 +0530

    feat(auth-service): add login service with jwt access token

--- auth-service/src/main/java/com/patternvault/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/patternvault/authservice/service/AuthService.java
@@ -1,22 +1,36 @@
 package com.patternvault.authservice.service;
 
+import com.patternvault.authservice.dto.LoginRequest;
+import com.patternvault.authservice.dto.LoginResponse;
 import com.patternvault.authservice.dto.RegisterRequest;
 import com.patternvault.authservice.dto.RegisterResponse;
 import com.patternvault.authservice.entity.User;
 import com.patternvault.authservice.exception.ConflictException;
 import com.patternvault.authservice.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 @Service
 public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtEncoder jwtEncoder;
 
-    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtEncoder jwtEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtEncoder = jwtEncoder;
     }
 
     @Transactional
@@ -33,5 +47,30 @@ public class AuthService {
         User saved = userRepository.save(user);
 
         return new RegisterResponse(saved.getId(), saved.getUsername(), saved.getEmail(), saved.getCreatedAt());
+    }
+
+    public LoginResponse login(LoginRequest req){
+        User user = userRepository.findByEmail(req.email()).orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials."));
+
+        if(!passwordEncoder.matches(req.password(), user.getPasswordHashed())){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials.");
+        }
+
+        return new LoginResponse(createAccessToken(user));
+    }
+
+    private String createAccessToken(User user){
+        Instant now = Instant.now();
+
+        JwtClaimsSet claimsSet = JwtClaimsSet.builder()
+                .subject(user.getId().toString())
+                .issuedAt(now)
+                .expiresAt(now.plus(15, ChronoUnit.MINUTES))
+                .build();
+
+        JwsHeader header = JwsHeader.with(MacAlgorithm.HS256).build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(header, claimsSet)).getTokenValue();
+
     }
 }
