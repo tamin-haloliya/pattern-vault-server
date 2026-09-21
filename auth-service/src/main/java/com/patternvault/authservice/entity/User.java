98f0d8c5c44809f38762c0a9c88e580ebbc14778
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-09-21 16:37:44 +0530

    feat(auth-service): add getters, no args constructor and parameterized constructor in user entity

--- auth-service/src/main/java/com/patternvault/authservice/entity/User.java
+++ auth-service/src/main/java/com/patternvault/authservice/entity/User.java
@@ -1,26 +1,40 @@
 package com.patternvault.authservice.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.Instant;
 import java.util.UUID;
 
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "users")
 public class User {
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Getter
     @Column(nullable = false, unique = true)
     private String username;
 
+    @Getter
     @Column(nullable = false, unique = true)
     private String email;
 
+    @Getter
     @Column(name = "password_hashed", nullable = false)
-    private String password;
+    private String passwordHashed;
 
+    @Getter
     @Column(nullable = false, updatable = false)
     private Instant createdAt = Instant.now();
+
+    public User(String username, String email, String passwordHashed) {
+        this.username = username;
+        this.email = email;
+        this.passwordHashed = passwordHashed;
+    }
 }
