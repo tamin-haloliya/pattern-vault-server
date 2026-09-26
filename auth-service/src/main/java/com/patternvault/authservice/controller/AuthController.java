0fac9bc285adb70bfb85e022bd189d5f6287bb92
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-09-26 20:38:22 +0530

    feat(auth-service): add controller for logoutAll

--- auth-service/src/main/java/com/patternvault/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/patternvault/authservice/controller/AuthController.java
@@ -5,7 +5,13 @@ import com.patternvault.authservice.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -34,6 +40,13 @@ public class AuthController {
     @PostMapping("/logout")
     public ResponseEntity<Void> logout(@Valid @RequestBody LogoutRequest req){
         this.authService.logout(req);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/logout-all")
+    public ResponseEntity<Void> logout(@AuthenticationPrincipal Jwt jwt){
+        UUID id = UUID.fromString(Objects.requireNonNull(jwt.getSubject()));
+        authService.logoutAll(id);
         return ResponseEntity.noContent().build();
     }
 }
