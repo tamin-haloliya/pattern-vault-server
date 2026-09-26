73ffc64b07e6c1f43d37f47391321c3383537bb7
tamin-haloliya <tamin.haloliya.dev@gmail.com>
2026-09-26 20:37:12 +0530

    feat(auth-service): add revokeAllRefreshToken operation in RefreshTokenRepository

--- auth-service/src/main/java/com/patternvault/authservice/repository/RefreshTokenRepository.java
+++ auth-service/src/main/java/com/patternvault/authservice/repository/RefreshTokenRepository.java
@@ -2,10 +2,17 @@ package com.patternvault.authservice.repository;
 
 import com.patternvault.authservice.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
     Optional<RefreshToken> findByTokenHash(String tokenHash);
+
+    @Modifying
+    @Query("UPDATE RefreshToken r SET r.revoked = true WHERE r.user.id = :id AND r.revoked = false")
+    void revokeAllRefreshTokens(@Param("id") UUID id);
 }
